ea2b4ea2b92187ab43cac5decfa445b2f67bd765
Francesco98Welcome <frankbenvenuti04@gmail.com>
2023-01-13 16:38:51 +0100

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,53 @@
+let h2 = document.querySelector('h2');
+let h3 = document.querySelector('h3');
+
+let numbersRandom = [];
+let numeriProvati= [];
+let numeriNonIndovinati = [];
+let numeriIndovinati = []; 
+
+numeroCasuale = '';
+
+for (let i = 0; i < 5; i++) {
+    const numeroCasuale = Math.floor(Math.random() * 100);
+    numbersRandom.push(numeroCasuale);
+    h2.innerHTML = numbersRandom;
+    console.log(numeroCasuale);
+}
+
+function sparisceNumeri () {
+    h2.innerHTML = '';
+}
+
+
+let numbers = '';
+
+function chiedoNumero() {
+
+    for (let i = 0; i < 5; i++) {
+    numbers = parseInt(prompt('Inserisci uno dei 5 numeri'));
+    numeriProvati.push(numbers);
+    console.log(numeriProvati);
+    }
+
+    for (let z = 0; z < numeriProvati.length; z++) {
+
+    if (!numbersRandom.includes(numeriProvati[z])) {
+        numeriNonIndovinati.push(numeriProvati[z]);
+        console.log(numeriNonIndovinati);
+        const differenza = numbersRandom.length - numeriNonIndovinati.length;
+        h2.innerHTML = 'i numeri indovinati sono ' + differenza;
+    } else {
+        numeriIndovinati.push(numeriProvati[z]);
+        console.log(numeriIndovinati);
+        h3.innerHTML = 'i numeri indovinati sono: ' + ' - ' + numeriIndovinati;
+    }
+  }
+}
+
+console.log(numbersRandom);
+
+
+setTimeout(chiedoNumero, 6000);
+setTimeout(sparisceNumeri, 5000);
+
